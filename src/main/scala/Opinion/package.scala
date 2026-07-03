8c65beae841cd62989afad3156d677f46c9df1a2
Dana Sofia Gomez Manrique <danagomez2816@gmail.com>
2026-07-02 20:42:37 -0500

    eliminacion de funciones repetidas

--- src/main/scala/Opinion/package.scala
+++ src/main/scala/Opinion/package.scala
@@ -11,39 +11,6 @@ package object Opinion {
   type AgentsPolMeasure =
     (SpecificBelief, Comete.DistributionValues) => Double
 
-  def uniformBelief(nags: Int): SpecificBelief =
-    Vector.tabulate(nags)(i => (i + 1).toDouble / nags.toDouble)
-
-  def allExtremeBelief(nags: Int): SpecificBelief = {
-    val mid = nags / 2
-    Vector.tabulate(nags)(i => if (i < mid) 0.0 else 1.0)
-  }
-
-  def midlyBelief(nags: Int): SpecificBelief = {
-    val middle = nags / 2
-
-    Vector.tabulate(nags) { i =>
-      if (i < middle)
-        math.max(0.25 - 0.01 * (middle - i - 1), 0.0)
-      else
-        math.min(0.75 - 0.01 * (middle - i), 1.0)
-    }
-  }
-
-  def allTripleBelief(nags: Int): SpecificBelief = {
-    val one3 = nags / 3
-    val two3 = (nags / 3) * 2
-
-    Vector.tabulate(nags) { i =>
-      if (i < one3) 0.0
-      else if (i >= two3) 1.0
-      else 0.5
-    }
-  }
-
-  def consensusBelief(b: Double)(nags: Int): SpecificBelief =
-    Vector.tabulate(nags)(_ => b)
-
   def betaFactor(bi: Double, bj: Double): Double =
     1.0 - math.abs(bj - bi)
 
